0226a0c847e34188363c935a68ace98f2289f75c
alexei <webdesfreelance@gmail.com>
2020-06-29 17:23:36 +0300

    firstsave

--- src/data/cityPhoto.js
+++ src/data/cityPhoto.js
@@ -1,24 +1,24 @@
 export const cities =  {
-    london: {
-      name: "London",
-      url:"https://images.unsplash.com/photo-1505761671935-60b3a7427bad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
-    },
-    chisinau: {
-      url: "https://sx-content-labs.sixt.io/chisinau-city-header.jpg",
-      name: "Chisinau"
-    },
-    moscow: {
-      url: "https://cdn.getyourguide.com/img/tour_img-1233054-98.jpg",
-      name: "Moscow"
-    },
-    ny: {
-      url: "https://d12dkjq56sjcos.cloudfront.net/pub/media/wysiwyg/newyork/01-city-landing/New-York-Skyline-Big-Bus-Tours-Jan-2018.jpg",
-      name: "New York"
-    },
-    paris: {
-      url: "https://imgix.bustle.com/uploads/shutterstock/2019/9/19/a49124d9-5f62-47a5-b5ec-8dd3a3066b30-shutterstock-1420728554.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70&dpr=2",
-      name: "Paris"
-    },
-  }
+  london: {
+    name: "London",
+    url:"https://cdn.contexttravel.com/image/upload/c_fill,q_60,w_2600/v1555943130/production/city/hero_image_11_1555943130.jpg"
+  },
+  chisinau: {
+    url: "https://winetours.md/pic/pages/18_single/cover_photo/moldova_landscape_cover_winetours_md__5____resize.jpg",
+    name: "Chisinau"
+  },
+  moscow: {
+    url: "https://allintravels.nl/wp-content/uploads/2018/09/destination-moscow-03.jpg",
+    name: "Moscow"
+  },
+  ny: {
+    url: "https://d12dkjq56sjcos.cloudfront.net/pub/media/wysiwyg/newyork/01-city-landing/New-York-Skyline-Big-Bus-Tours-Jan-2018.jpg",
+    name: "New York"
+  },
+  paris: {
+    url: "https://imgix.bustle.com/uploads/shutterstock/2019/9/19/a49124d9-5f62-47a5-b5ec-8dd3a3066b30-shutterstock-1420728554.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70&dpr=2",
+    name: "Paris"
+  },
+}
 
-   
+ 
