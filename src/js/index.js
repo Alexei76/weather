18f6d4d60795cb69b6321c89bc23709d116a0905
alexei <webdesfreelance@gmail.com>
2020-06-30 13:36:41 +0300

    css

--- src/js/index.js
+++ src/js/index.js
@@ -46,7 +46,7 @@ function renderCityInfoBox(data) {
     container.classList.add('city-info-box');
 
     const cityName = document.createElement('div');
-    cityName.innerText = `${name}: ${main.temp}`
+    cityName.innerText = `${name}: ${Math.round(parseFloat(main.temp))}`
 
     const celsius = document.createElement('span')
     celsius.innerHTML = '&#x2103;'
