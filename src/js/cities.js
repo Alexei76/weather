84bf88d13bb7a1a7a6dadb7743fecc208a06959e
alexei <webdesfreelance@gmail.com>
2020-06-30 12:05:14 +0300

    citiesjschanges

--- src/js/cities.js
+++ src/js/cities.js
@@ -1,15 +1,18 @@
-import { getWeatherByCity } from './api';
-import { cities } from '../data/cityPhoto';
-
-
 function renderCity(data) {
     const container = document.querySelector('.cities-list');
+
     const cityContainer = document.createElement('div')
     cityContainer.classList.add('city_container');
-    const cityName = document.createElement('span');
-    cityContainer.innerText = data.main.temp;
-    cityContainer.append(cityName)
-    cityName.innerText = `: ${data.name}`;
+
+    const cityTemp = document.createElement('span');
+    cityTemp.classList.add('city_temp');
+
+    cityContainer.innerText = ` ${data.name} :`;
+
+    cityContainer.append(cityTemp)
+
+    cityTemp.innerText = data.main.temp;
+
     container.append(cityContainer);
 }
 
