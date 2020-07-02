2f8b885f3c303c014816fad366cc88c953894dee
alexei <webdesfreelance@gmail.com>
2020-07-02 17:20:01 +0300

    fetch

--- src/js/api.js
+++ src/js/api.js
@@ -3,3 +3,7 @@ const API_KEY = '10596381ff4bbaf68c11b7be4338d315';
 export function getWeatherByCity(city) {
     return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&&appid=${API_KEY}`).then(rsp => rsp.json())
 }
+
+export function getForecast(city) {
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`).then(rsp => rsp.json())
+}
