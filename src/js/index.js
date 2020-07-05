e8488ad2aec4b2a598c4181c436e72ddb03aee5c
alexei <webdesfreelance@gmail.com>
2020-07-05 23:58:35 +0300

    save

--- src/js/index.js
+++ src/js/index.js
@@ -63,53 +63,53 @@ function renderForcastDay(item){
     const weatherDes = document.createElement('div');
     weatherDes.innerText = item.weather[0].description;
     for (let i = 500; i <= 504; i++) {
-        if (data.weather[0].id === i) {
+        if (item.weather[0].id === i) {
             clouds.setAttribute('src', 'http://openweathermap.org/img/wn/10d@2x.png');
         }
     }
     for (let l = 520; l <= 531; l++) {
-        if (data.weather[0].id === l) {
+        if (item.weather[0].id === l) {
             clouds.setAttribute('src', 'http://openweathermap.org/img/wn/09d@2x.png');
         }
     }
 
-    if (data.weather[0].id === 501) {
+    if (item.weather[0].id === 501) {
         clouds.setAttribute('src', 'http://openweathermap.org/img/wn/13d@2x.png');
     }
 
-    if (data.weather[0].id === 800) {
+    if (item.weather[0].id === 800) {
         clouds.setAttribute('src', 'http://openweathermap.org/img/wn/01d@2x.png');
     }
 
-    if (data.weather[0].id === 801) {
+    if (item.weather[0].id === 801) {
         clouds.setAttribute('src', 'http://openweathermap.org/img/wn/02d@2x.png');
     }
-    if (data.weather[0].id === 802) {
+    if (item.weather[0].id === 802) {
         clouds.setAttribute('src', 'http://openweathermap.org/img/wn/03d@2x.png');
     }
     for (let j = 803; j <= 804; j++) {
-        if (data.weather[0].id === j) {
+        if (item.weather[0].id === j) {
             clouds.setAttribute('src', 'http://openweathermap.org/img/wn/04d@2x.png');
         }
     }
     for (let a = 701; a <= 781; a++) {
-        if (data.weather[0].id === a) {
+        if (item.weather[0].id === a) {
             clouds.setAttribute('src', 'http://openweathermap.org/img/wn/50d@2x.png');
         }
     }
     for (let s = 600; s <= 622; s++) {
-        if (data.weather[0].id === s) {
+        if (item.weather[0].id === s) {
             clouds.setAttribute('src', 'http://openweathermap.org/img/wn/13d@2x.png');
         }
     }
     for (let d = 300; d <= 321; d++) {
-        if (data.weather[0].id === d) {
+        if (item.weather[0].id === d) {
             clouds.setAttribute('src', 'http://openweathermap.org/img/wn/09d@2x.png');
         }
     }
 
     for (let b = 200; b <= 232; b++) {
-        if (data.weather[0].id === b) {
+        if (item.weather[0].id === b) {
             clouds.setAttribute('src', 'http://openweathermap.org/img/wn/11d@2x.png');
         }
     }
